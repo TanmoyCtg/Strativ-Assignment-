38f7b6ed895ec3b106e49564eeabbb7bb7d0a425
iftelhar <x@gmail.com>
2021-04-06 18:00:41 +0600

    added new url point for add country

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('country/', views.CountryList.as_view()),
-    path('country/1/' , views.getCountry.as_view())
+    path('country/1/' , views.getCountry.as_view()),
+    path('country/add/', views.CreatingNewCountry.as_view())
 ]
