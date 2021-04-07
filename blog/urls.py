d0fa8bf4c43af2bc58756f9a74c8c701f23a532d
iftelhar <x@gmail.com>
2021-04-07 23:11:39 +0600

    added two new url

--- blog/urls.py
+++ blog/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path('country/', views.CountryList.as_view()),
     path('country/1/' , views.getCountry.as_view()),
     path('country/add/', views.CreatingNewCountry.as_view()),
-    path('country/update/<int:pk>/', views.updateDeleteCountry.as_view())
+    path('country/update/<int:pk>/', views.updateDeleteCountry.as_view()),
+    
+    path('country/search/', views.SearchResultsView.as_view(), name='search'),
+    path('countrypage/', views.HomePageView.as_view(), name='home'),
 ]
