e353937c21b50ed8087bebccc5024698110f44ab
iftelhar <x@gmail.com>
2021-04-11 20:19:57 +0600

    added api-auth for api authentication check

--- django_project/urls.py
+++ django_project/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('blog/', include('blog.urls')),
     
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
